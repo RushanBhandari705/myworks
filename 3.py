75d0aee0d2da501298e3d560c13a7ad265528e28
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-03-18 18:13:35 +0545

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,8 @@
+#write a program that creates a new list containing all the elements placed on even positions of the original list.
+# [43,23,21,44,56,75] -> [23,44,75]
+original_list = [43,23,21,44,56,75]
+new_list = []
+for i in range(1, len(original_list), 2):
+    new_list.append(original_list[i])
+print(new_list)
+
