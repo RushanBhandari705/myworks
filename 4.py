a72ebacb830e260d75a4dda8515b140cf30bf272
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-03-18 18:13:50 +0545

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,8 @@
+#Write a program that creates a new list containing all the elements placed on odd positions of the original list.
+# [12,22,32,42,52,62] -> [12,32,52]
+original_list = [12,22,32,42,52,62]
+new_list = []
+for i in range(0, len(original_list), 2):
+    new_list.append(original_list[i])
+print(new_list)
+
