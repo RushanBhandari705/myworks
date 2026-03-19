4795c2a84f4fa2a2c6c51e5913092401cd05c086
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-03-19 20:10:28 +0545

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,10 @@
+arr = [10, 20, 30, 40, 50]
+
+key = int(input("Enter number to search: "))
+
+for i in range(len(arr)):
+    if arr[i] == key:
+        print("Found at index", i)
+        break
+else:
+    print("Not found")
