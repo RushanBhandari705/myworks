5bca1a670248c8d5f9138e7f10f78a6c25476c1d
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-03-30 09:13:53 +0545

    Update list2 for element-wise addition

--- 7.py
+++ 7.py
@@ -1,7 +1,7 @@
 #Write a program that performs element-wise addition of two lists of numbers having the same length and outputs another list containing the sums.
 # [1,2,3,4,5,6] + [1,2,3,4,5,6] -> [2,4,6,8,10,12]
 list1 = [1,2,3,4,5,6]
-list2 = [1,2,3,4,5,6]
+list2 = [1,3,4,5,6,7]
 new_list = []
 for i in range(len(list1)):
     new_list.append(list1[i] + list2[i])
