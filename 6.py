b154ddd2e16881ac7143af6985a1959deea3b98f
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-03-30 09:13:20 +0545

    Update second list element from '#' to '!'

--- 6.py
+++ 6.py
@@ -1,7 +1,7 @@
 #Write a program that contains two lists by alternating elements.
 # ['A','H','M'], ['@','#','&'] -> ['A','@','H','#','M','&']
 list1 = ['A','H','M']
-list2 = ['@','#','&']
+list2 = ['@','!','&']
 new_list = []
 for i in range(len(list1)):
     new_list.append(list1[i])
