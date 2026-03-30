33832a7a2b8317b0fd632de69fc1f57a5b77773b
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-03-30 09:12:59 +0545

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,8 +1,8 @@
 #Write a program that contains two lists of equal length and alternatingly taking elements. eg
 # ['a','b','c'], [1,2,3] -> ['a',1,'b',2,'c',3]
 
-list1 = ['a','b','c']
-list2 = [1,2,3]
+list1 = ['a','b','c','d']
+list2 = [1,2,3,4]
 new_list = []
 for i in range(len(list1)):
     new_list.append(list1[i])
