9fb4b2e44b9fcf0406de22e7087c92bc2baaf2eb
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-04-06 20:44:48 +0545

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 #Write a program that prints out all the elements of a list which are divisible by 2 and 3. Use the following list:
 # a = [2,3,6,8,9,12,15,18,24,22,33,112]
 
-a = [2,3,6,8,9,12,56,46,4,22,33,112]
+a = [2,3,6,8,9,56,46,4,22,33,112]
 for i in a:
     if i % 2 == 0 and i % 3 == 0:
         print(i)
