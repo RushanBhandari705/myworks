94767cb6fd7253fa838a0c34417aad6d7a91ccdf
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-04-12 20:38:41 +0545

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,5 +1,4 @@
-#Given two lists of alphabets of equal length, perform element wise concat and store the result in a new list.
-#['a','b','c','d','e'] + ['f','g','h','i','j'] -> ['af','bg','ch','di','ej']
+
 list1 = ['a','b','c','d','e']
 list2 = ['f','g','h','i','j']
 new_list = []
